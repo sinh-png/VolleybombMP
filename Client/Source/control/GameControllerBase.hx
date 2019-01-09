2892d8583d7588ad489a97423c5139a57656e396
melonin <hazagames404@gmail.com>
2019-01-09 20:31:24 +0700

    Update physics.

--- Client/Source/control/GameControllerBase.hx
+++ Client/Source/control/GameControllerBase.hx
@@ -2,7 +2,9 @@ package control;
 
 import control.input.InputControllerBase;
 import control.input.KeyboardInput;
-import nape.callbacks.CbType;
+import nape.callbacks.InteractionType;
+import nape.callbacks.PreFlag;
+import nape.callbacks.PreListener;
 import nape.geom.Vec2;
 import nape.phys.Body;
 import nape.phys.BodyType;
@@ -11,15 +13,6 @@ import nape.space.Space;
 
 @:access(control.input.InputControllerBase)
 class GameControllerBase {
-	
-	public static inline var WORLD_WIDTH = 640;
-	public static inline var WORLD_HEIGHT = 480;
-	public static inline var GROUND_Y = 445; 
-	
-	static var CBTYPE_WALL:CbType = new CbType();
-	static var CBTYPE_PASSTHROUGH:CbType = new CbType();
-	
-	//
 	
 	public var mode(default, null):GameMode;
 	var input(default, set):InputControllerBase;
@@ -32,6 +25,15 @@ class GameControllerBase {
 		input = new KeyboardInput();
 		
         space = new Space(Vec2.weak(0, 400));
+		space.listeners.add(new PreListener(
+			InteractionType.COLLISION,
+			WorldValue.CBTYPE_WALL,
+			WorldValue.CBTYPE_PASSTHROUGH,
+			function(_) return PreFlag.IGNORE, 
+			0,
+			true
+		));
+		
 		leftPlayer = new PlayerController(space, Main.instance.gameState.tilemap.leftPlayer, input.left);
 		rightPlayer = new PlayerController(space, Main.instance.gameState.tilemap.rightPlayer, input.right);
 		
@@ -39,19 +41,19 @@ class GameControllerBase {
 	}
 	
 	function initWalls():Void {
-		var wall = new Body(BodyType.STATIC, Vec2.weak( -1, WORLD_HEIGHT / 2));
-		wall.shapes.push(new Polygon(Polygon.box(1, WORLD_HEIGHT)));
-		//wall.cbTypes.add(CBTYPE_WALL);
+		var wall = new Body(BodyType.STATIC, Vec2.weak( -1, WorldValue.HEIGHT / 2));
+		wall.shapes.push(new Polygon(Polygon.box(1, WorldValue.HEIGHT)));
+		wall.cbTypes.add(WorldValue.CBTYPE_WALL);
 		wall.space = space;
 		
-		wall = new Body(BodyType.STATIC, Vec2.weak(641, WORLD_HEIGHT / 2));
-		wall.shapes.push(new Polygon(Polygon.box(1, WORLD_HEIGHT)));
-		//wall.cbTypes.add(CBTYPE_WALL);
+		wall = new Body(BodyType.STATIC, Vec2.weak(WorldValue.WIDTH + 1, WorldValue.HEIGHT / 2));
+		wall.shapes.push(new Polygon(Polygon.box(1, WorldValue.HEIGHT)));
+		wall.cbTypes.add(WorldValue.CBTYPE_WALL);
 		wall.space = space;
 		
-		wall = new Body(BodyType.STATIC, Vec2.weak(WORLD_WIDTH / 2, -1));
-		wall.shapes.push(new Polygon(Polygon.box(WORLD_WIDTH, 1)));
-		//wall.cbTypes.add(CBTYPE_WALL);
+		wall = new Body(BodyType.STATIC, Vec2.weak(WorldValue.WIDTH / 2, -1));
+		wall.shapes.push(new Polygon(Polygon.box(WorldValue.WIDTH, 1)));
+		wall.cbTypes.add(WorldValue.CBTYPE_WALL);
 		wall.space = space;
 	}
 	
--- Client/Source/control/PlayerController.hx
+++ Client/Source/control/PlayerController.hx
@@ -27,13 +27,11 @@ class PlayerController {
 		
 		body = new Body();
 		if (display.left) {
-			body.position.x = GameControllerBase.WORLD_WIDTH / 2 - 200;
 			new Polygon([Vec2.weak(10, -10.5), Vec2.weak(58, -9.5), Vec2.weak(11.5, -38) ]).body = body;
 			new Polygon([Vec2.weak(62.5, -74), Vec2.weak(43, -86.5), Vec2.weak(24, -86.5), Vec2.weak(63.5, -44), Vec2.weak(69.5, -50) ]).body = body;
 			new Polygon([Vec2.weak(4, -43.5), Vec2.weak(11.5, -38), Vec2.weak(58, -9.5), Vec2.weak(63.5, -14), Vec2.weak(63.5, -44), Vec2.weak(24, -86.5), Vec2.weak(13.5, -80), Vec2.weak(6.5, -65) ]).body = body;
 			
 		} else {
-			body.position.x = GameControllerBase.WORLD_WIDTH / 2 + 200;
 			new Polygon([ Vec2.weak(47, -89.5), Vec2.weak(29, -89.5), Vec2.weak(59.5, -42), Vec2.weak(66.5, -49), Vec2.weak(61.5, -76) ]).body = body;
 			new Polygon([ Vec2.weak(13, -82.5), Vec2.weak(5.5, -68), Vec2.weak(7.5, -38), Vec2.weak(16, -12.5), Vec2.weak(58, -13.5), Vec2.weak(61.5, -15), Vec2.weak(29, -89.5), Vec2.weak(28, -89.5) ]).body = body;
 			new Polygon([ Vec2.weak(7.5, -17), Vec2.weak(16, -12.5), Vec2.weak(7.5, -38) ]).body = body;
@@ -42,14 +40,17 @@ class PlayerController {
 		}
 		
 		var material = new Material(0.75, 5, 5, 10, 0);
+		body.translateShapes(Vec2.weak( -display.originX, display.originY));
 		body.setShapeMaterials(material);
 		body.gravMass = 100;
 		body.allowRotation = false;
+		body.cbTypes.add(WorldValue.CBTYPE_PASSTHROUGH);
 		body.space = space;
 	}
 	
 	public function reset():Void {
-		display.y = body.position.y = 200;
+		display.x = body.position.x = WorldValue.WIDTH / 2 + 200 * (left ? -1 : 1);
+		display.y = body.position.y = getMaxY();
 	}
 	
 	public function update(delta:Float):Void {
@@ -91,6 +92,19 @@ class PlayerController {
 			case Direction.NONE: 		body.velocity.x = 0;
 		}
 		
+		var offset = 20;
+		if (left) {
+			if (body.position.x < display.originX - offset)
+				body.position.x = display.originX - offset;
+			else if (body.position.x > 250)
+				body.position.x = 250;
+		} else {
+			if (body.position.x > WorldValue.WIDTH - display.originX + offset)
+				body.position.x = WorldValue.WIDTH - display.originX + offset;
+			else if (body.position.x < 390)
+				body.position.x = 390;
+		}
+		
 		updateDisplay();
 	}
 	
@@ -104,7 +118,7 @@ class PlayerController {
 	}
 	
 	function getMaxY():Float {
-		return GameControllerBase.GROUND_Y - display.height / 2;
+		return WorldValue.GROUND_Y - display.height / 2;
 	}
 	
 }
