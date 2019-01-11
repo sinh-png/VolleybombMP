d72555e06535ba0b8854733dc39e6b2c0322e035
melonin <hazagames404@gmail.com>
2019-01-11 10:31:44 +0700

    Update main menu.

---
+++ Client/Source/display/menu/MainMenu.hx
@@ -0,0 +1,127 @@
+package display.menu;
+
+import motion.Actuate;
+import openfl.display.Sprite;
+import openfl.events.MouseEvent;
+import openfl.text.TextField;
+import openfl.text.TextFormat;
+import openfl.ui.Mouse;
+import openfl.ui.MouseCursor;
+
+class MainMenu extends Sprite {
+	
+	static inline var VS_COMP = "Vs Comp";
+	static inline var VS_LOCAL = "2 Players Local";
+	static inline var HOST = "Create Online Game";
+	static inline var JOIN = "Join Online Game";
+	
+	//
+	
+	var buttons:Array<Button> = [];
+
+	public function new() {
+		super();
+		
+		var bgWidth = 230;
+		var paddingY = 10;
+		var buttonSpacing = 5;
+		var buttonTexts = [ VS_COMP, VS_LOCAL, HOST, JOIN ];
+		var button:Button = null;
+		for (i in 0...buttonTexts.length) {
+			button = new Button(buttonTexts[i]);
+			button.x = (bgWidth - button.width) / 2;
+			button.y = paddingY + (button.height + buttonSpacing) * i;
+			button.onClickCB = onButtonClicked;
+			buttons.push(button);
+			addChild(button);
+		}
+		
+		var bgHeight = button.y + button.height + paddingY;
+		graphics.beginFill(0x0, 0.9);
+		graphics.drawRoundRect(0, 0, bgWidth, bgHeight, 8, 8);
+		graphics.endFill();
+		
+		addEventListener(MouseEvent.MOUSE_MOVE, function(_) {
+			for (button in buttons) {
+				if (button.isHovered()) {
+					if (!button.focused)
+						button.onFocus();
+				} else {
+					if (button.focused)
+						button.onOut();
+				}
+			}
+		});
+		addEventListener(MouseEvent.ROLL_OUT, function(_) {
+			for (button in buttons)
+				button.onOut();
+		});
+		addEventListener(MouseEvent.CLICK, function(_) {
+			if (Button.focusedButton != null)
+				Button.focusedButton.onClick();
+		});
+	}
+	
+	function onButtonClicked(mode:String):Void {
+		switch(mode) {
+			case VS_COMP:
+				Main.instance.gameState.activate(GameMode.LOCAL(true));
+				
+			case VS_LOCAL:
+				Main.instance.gameState.activate(GameMode.LOCAL(false));
+			
+			case HOST:
+				
+			case JOIN:
+				
+		}
+	}
+	
+}
+
+class Button extends TextField {
+	
+	static inline var outAlpha = 0.6;
+	
+	public static var focusedButton(default, null):Button;
+	
+	//
+	
+	public var onClickCB:String-> Void;
+	public var focused(get, never):Bool;
+	
+	public function new(text:String) {
+		super();
+		
+		defaultTextFormat = new TextFormat(R.defaultFont, 30, 0xFFFFFF, true);
+		this.text = text;
+		width = textWidth + 4;
+		height = textHeight + 4;
+		alpha = outAlpha;
+		mouseEnabled = false;
+	}
+	
+	public function isHovered():Bool {
+		return mouseX >= 0 && mouseX <= width && mouseY >= 0 && mouseY <= height;
+	}
+	
+	public function onFocus():Void {
+		Actuate.tween(this, 0.6, { alpha: 1 } );
+		focusedButton = this;
+		Mouse.cursor = MouseCursor.BUTTON;
+	}
+	
+	public function onOut():Void {
+		Actuate.tween(this, 1, { alpha: outAlpha } );
+		focusedButton = null;
+		Mouse.cursor = MouseCursor.AUTO;
+	}
+	
+	public function onClick():Void {
+		if (onClickCB != null)
+			onClickCB(text);
+	}
+	
+	inline function get_focused():Bool return focusedButton == this;
+	
+}
--- Client/Source/display/menu/MenuState.hx
+++ Client/Source/display/menu/MenuState.hx
@@ -1,15 +1,11 @@
 package display.menu;
 
-import control.net.Connection;
-import control.net.Room;
 import openfl.display.Bitmap;
-import openfl.display.Sprite;
-import openfl.events.MouseEvent;
 
 class MenuState extends StateBase {
 	
 	var background:Bitmap;
-	var modeMenu:Sprite;
+	var menu:MainMenu;
 
 	public function new() {
 		super();
@@ -20,74 +16,10 @@ class MenuState extends StateBase {
 		baseWidth = background.width;
 		baseHeight = background.height;
 		
-		initModeButtons();
-	}
-	
-	
-	function initModeButtons():Void {
-		modeMenu = new Sprite();
-		addChild(modeMenu);
-		
-		var bgWidth = 200;
-		var paddingY = 10;
-		var buttonSpacing = 5;
-		var buttonTexts = [ GameModeButton.VS_COMP, GameModeButton.VS_LOCAL, GameModeButton.HOST, GameModeButton.JOIN ];
-		var button:GameModeButton = null;
-		for (i in 0...buttonTexts.length) {
-			button = new GameModeButton(buttonTexts[i]);
-			button.x = (bgWidth - button.width) / 2;
-			button.y = paddingY + (button.height + buttonSpacing) * i;
-			button.onClickCB = onModeRequest;
-			modeMenu.addChild(button);
-		}
-		
-		var bgHeight = button.y + button.height + paddingY;
-		var g = modeMenu.graphics;
-		g.beginFill(0x0, 0.9);
-		g.drawRoundRect(0, 0, bgWidth, bgHeight, 8, 8);
-		g.endFill();
-		modeMenu.x = (baseWidth - bgWidth) / 2;
-		modeMenu.y = (baseHeight - modeMenu.height) / 2;
-		
-		modeMenu.addEventListener(MouseEvent.MOUSE_MOVE, function(_) {
-			for (i in 0...modeMenu.numChildren) {
-				var button:GameModeButton = cast modeMenu.getChildAt(i);
-				if (button.isHovered()) {
-					if (!button.focused)
-						button.onFocus();
-				} else {
-					if (button.focused)
-						button.onOut();
-				}
-			}
-		});
-		modeMenu.addEventListener(MouseEvent.ROLL_OUT, function(_) {
-			for (i in 0...modeMenu.numChildren)
-				cast(modeMenu.getChildAt(i), GameModeButton).onOut();
-		});
-		modeMenu.addEventListener(MouseEvent.CLICK, function(_) {
-			if (GameModeButton.focusedButton != null)
-				GameModeButton.focusedButton.onClick();
-		});
-	}
-	
-	function onModeRequest(mode:String):Void {
-		switch(mode) {
-			case GameModeButton.VS_COMP:
-				Main.instance.gameState.activate(GameMode.LOCAL(true));
-				
-			case GameModeButton.VS_LOCAL:
-				Main.instance.gameState.activate(GameMode.LOCAL(false));
-			
-			case GameModeButton.HOST:
-				Room.create(
-					function(roomID) trace(roomID),
-					function (answer) trace(answer),
-					function(con) trace("other player joined!") 
-				);
-				
-			case GameModeButton.JOIN:
-		}
+		menu = new MainMenu();
+		menu.x = (baseWidth - menu.width) / 2;
+		menu.y = (baseHeight - menu.height) / 2;
+		addChild(menu);
 	}
 	
 }
