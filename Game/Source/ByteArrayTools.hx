e516b2d53f8a97ad2db93b99ddb36f09a26f66e3
melonin <hazagames404@gmail.com>
2019-01-11 15:31:50 +0700

    Add ByteArrayTool.

---
+++ Game/Source/ByteArrayTools.hx
@@ -0,0 +1,28 @@
+package;
+
+import haxe.io.Bytes;
+import js.html.ArrayBuffer;
+import openfl.utils.ByteArray;
+
+class ByteArrayTools {
+	
+	static var pool:Array<ByteArray> = [];
+
+	public static function get():ByteArray {
+		return pool.length > 0 ? pool.pop() : new ByteArray();
+	}
+	
+	public static function put(bytes:ByteArray):Void {
+		bytes.clear();
+		pool.push(bytes);
+	}
+	
+	public static inline function toArrayBuffer(bytes:ByteArray):ArrayBuffer {
+		return cast @:privateAccess cast(bytes, Bytes).b;
+	}
+	
+	public static inline function fromArrayBuffer(buffer:ArrayBuffer):ByteArray {
+		return @:privateAccess new Bytes(buffer);
+	}
+	
+}
