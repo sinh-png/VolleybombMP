7dfe26873374e49a95fa1fa19617faf081dbf7b1
melonin <hazagames404@gmail.com>
2019-01-14 12:40:19 +0700

    Rename Interpolator.apply to run.

--- Game/Source/control/net/Interpolator.hx
+++ Game/Source/control/net/Interpolator.hx
@@ -2,7 +2,7 @@ package control.net;
 
 class Interpolator {
 	
-	public static inline function apply(from:Float, to:Float):Float {
+	public static inline function run(from:Float, to:Float):Float {
 		var distance = to - from;
 		return from + distance * Math.min(Math.abs(distance) / 50, 1);
 	}
