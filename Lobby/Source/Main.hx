b83a3cb221e62eb486c3eb6d12083748ed335d23
melonin <hazagames404@gmail.com>
2019-01-24 02:29:52 +0700

    Update local testing.

--- Lobby/Source/Main.hx
+++ Lobby/Source/Main.hx
@@ -8,22 +8,25 @@ class Main  {
 	
 	static function main() {
 		var app = Lib.require('express')();
-		var whitelist = [ 'http://localhost:2000', Node.process.env.get('ALLOWED_ORIGIN') ];
+		#if !localTest
+		var whitelist = [ Node.process.env.get('ALLOWED_ORIGIN') ];
 		app.use(Lib.require('cors')({
 			credentials: true,
 			origin: function(origin, callback:Dynamic) {
 				if (whitelist.indexOf(origin) > -1)
 					callback(null, true);
 				else
-					callback(new Error('Not allowed by CORS'));
+					callback(new Error('Not allowed by CORS.'));
 			}
 		}));
-		
-		app.get('/iceServers', IceExpress.handler);
+		#end
 		
 		var server = Lib.require('http').Server(app);
 		var port = Node.process.env.exists('PORT') ? Std.parseInt(Node.process.env.get('PORT')) : 5000;
 		server.listen(port, function () trace('Started web server on port: $port'));
+		
+		app.get('/iceServers', IceExpress.handler);
+		
 		Room.init(server);
 	}
 	
