32ac770f9f0048054ba1718339674795550993ea
melonin <hazagames404@gmail.com>
2019-01-24 17:22:46 +0700

    Remove ByteArray pooling.

--- Game/Source/net/ByteArrayTools.hx
+++ Game/Source/net/ByteArrayTools.hx
@@ -6,17 +6,6 @@ import openfl.utils.ByteArray;
 
 class ByteArrayTools {
 	
-	static var pool:Array<ByteArray> = [];
-
-	public static inline function get():ByteArray {
-		return pool.length > 0 ? pool.pop() : new ByteArray();
-	}
-	
-	public static inline function put(bytes:ByteArray):Void {
-		bytes.clear();
-		pool.push(bytes);
-	}
-	
 	public static inline function toArrayBuffer(bytes:ByteArray):ArrayBuffer {
 		return cast @:privateAccess cast(bytes, Bytes).b;
 	}
